c22c8410f403c31fe3435d47af457e94f6a6b450
Paige Sun <psun@tribalscale.com>
2019-03-15 12:46:49 -0400

    Add a NameFormDialog

---
+++ src/components/Dialogs/NameFormDialog.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import socket from '../utils/socket'
+
+const NameFormDialog = ({handleClose, handleClickOpen, changeUserName}) => {
+  const [open, setOpen] = useState(false)
+
+  handleClickOpen = () => {
+    setOpen(true)
+  };
+
+  handleClose = () => {
+    setOpen(false)
+  };
+
+  return (
+    <div>
+      <Button className="profile-name-button"
+        type='light'
+        color='#ffffff'
+        onClick={setOpen}
+      >
+        Profile
+        </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title">
+          What should we call you?
+          </DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            type="text"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Okay!
+            </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
+
+export default NameFormDialog
